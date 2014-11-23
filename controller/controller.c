02ed062d0e4dfd5578328954c17ec25ea5fcc3f9
Claudiu Ghioc VM <claudiu.ghioc@gmail.com>
2014-11-23 02:50:46 -0800

    Move all the flow installation steps in one function
    Signed-off-by: Claudiu Ghioc VM <claudiu.ghioc@gmail.com>

--- controller/controller.c
+++ controller/controller.c
@@ -30,9 +30,53 @@ static inline void print_mac(char *mac)
 	printf("\n");
 }
 
-static void set_rule(void)
-{
 
+/* Install a rule */
+static void set_rule(int sw, char *src_mac, char *dst_mac, int port,
+		int tcp_src_port, int tcp_dst_port)
+{
+	int res = 0;
+	openflow_actions *actions;
+	struct ofp_match match;
+	buffer *buff;
+
+
+	/* Install rule to forward flow on the next port */
+	memset(&match, 0, sizeof(struct ofp_match));
+	match.wildcards = OFPFW_ALL;
+
+	if (dst_mac) {
+		match.wildcards ^= OFPFW_DL_DST;
+		memcpy(match.dl_dst, dst_mac, 6);
+	}
+	if (src_mac) {
+		match.wildcards ^= OFPFW_DL_SRC;
+		memcpy(match.dl_src, src_mac, 6);
+	}
+	if (tcp_src_port > 0) {
+		match.wildcards ^= OFPFW_TP_SRC;
+		match.tp_src = tcp_src_port;
+	}
+	if (tcp_dst_port > 0) {
+		match.wildcards ^= OFPFW_TP_DST;
+		match.tp_dst = tcp_dst_port;
+	}
+	match.wildcards ^= OFPFW_NW_PROTO;
+	match.nw_proto = IPPROTO_TCP;
+	match.wildcards ^= OFPFW_DL_TYPE;
+	match.dl_type = 0x0800;
+
+	actions = create_actions();
+	append_action_output(actions, port, 0);
+
+	buff = create_flow_mod(get_transaction_id(), match, get_cookie(),
+			OFPFC_ADD, OFP_FLOW_PERMANENT, OFP_FLOW_PERMANENT,
+			0xffff, -1, OFPP_NONE, OFPFF_SEND_FLOW_REM, actions);
+	if (!(res = send_openflow_message(sw, buff)))
+		printf("Error installing rule, res %d\n", res);
+
+	delete_actions(actions);
+	free_buffer(buff);
 }
 
 
@@ -75,33 +119,7 @@ static void packet_forward(int sw, char *src_mac, char *dst_mac, int in_port,
 	printf("Must forward this to port %d\n", port);
 
 	/* Install rule to forward flow on the next port */
-	memset(&match, 0, sizeof(struct ofp_match));
-	match.wildcards = OFPFW_ALL;
-	match.wildcards ^= OFPFW_DL_DST;
-	match.wildcards ^= OFPFW_DL_SRC;
-	match.wildcards ^= OFPFW_TP_SRC;
-	match.wildcards ^= OFPFW_TP_DST;
-	memcpy(match.dl_dst, dst_mac, 6);
-	memcpy(match.dl_src, src_mac, 6);
-	match.tp_src = tcp_src_port;
-	match.tp_dst = tcp_dst_port;
-	match.wildcards ^= OFPFW_NW_PROTO;
-	match.nw_proto = IPPROTO_TCP;
-	match.wildcards ^= OFPFW_DL_TYPE;
-	match.dl_type = 0x0800;
-
-	actions = create_actions();
-	append_action_output(actions, port, 0);
-
-	buff = create_flow_mod(get_transaction_id(), match, get_cookie(),
-			OFPFC_ADD, OFP_FLOW_PERMANENT, OFP_FLOW_PERMANENT,
-			0xffff, -1, OFPP_NONE, OFPFF_SEND_FLOW_REM, actions);
-	if (!(res = send_openflow_message(sw, buff)))
-		printf("Error installing alternate rule, res %d\n", res);
-
-	delete_actions(actions);
-	free_buffer(buff);
-
+	set_rule(sw, src_mac, dst_mac, port, tcp_src_port, tcp_dst_port);
 }
 
 /* Install rule to forward flow to the host */
@@ -116,60 +134,11 @@ static void packet_to_host(int sw, char *src_mac, char *dst_mac, int in_port,
 	printf("Must forward this to my host\n");
 
 	/* Install rule to forward flow on the switch's host */
-	memset(&match, 0, sizeof(struct ofp_match));
-	match.wildcards = OFPFW_ALL;
-	match.wildcards ^= OFPFW_DL_DST;
-	memcpy(match.dl_dst, dst_mac, 6);
-	match.wildcards ^= OFPFW_TP_DST;
-	match.tp_dst = tcp_dst_port;
-	match.wildcards ^= OFPFW_TP_SRC;
-	match.tp_src = tcp_src_port;
-	match.wildcards ^= OFPFW_NW_PROTO;
-	match.nw_proto = IPPROTO_TCP;
-	match.wildcards ^= OFPFW_DL_TYPE;
-	match.dl_type = 0x0800;
-
-	actions = create_actions();
-	append_action_output(actions, 1, 0);
-
-	buff = create_flow_mod(get_transaction_id(), match, get_cookie(),
-			OFPFC_ADD, OFP_FLOW_PERMANENT, OFP_FLOW_PERMANENT,
-			0xffff, -1, OFPP_NONE, OFPFF_SEND_FLOW_REM, actions);
-	if (!(res = send_openflow_message(sw, buff)))
-		printf("Error installing to host rule, res %d\n", res);
-
-	delete_actions(actions);
-	free_buffer(buff);
-
+	set_rule(sw, src_mac, dst_mac, 1, tcp_src_port, tcp_dst_port);
 
 	/* Install rule to forward packets back to the sender */
 	printf("Also install rule to go back through %d\n", in_port);
-	memset(&match, 0, sizeof(struct ofp_match));
-	match.wildcards = OFPFW_ALL;
-	match.wildcards ^= OFPFW_DL_DST;
-	match.wildcards ^= OFPFW_DL_SRC;
-	match.wildcards ^= OFPFW_TP_SRC;
-	match.wildcards ^= OFPFW_TP_DST;
-	memcpy(match.dl_dst, src_mac, 6);
-	memcpy(match.dl_src, dst_mac, 6);
-	match.tp_src = tcp_dst_port;
-	match.tp_dst = tcp_src_port;
-	match.wildcards ^= OFPFW_NW_PROTO;
-	match.nw_proto = IPPROTO_TCP;
-	match.wildcards ^= OFPFW_DL_TYPE;
-	match.dl_type = 0x0800;
-
-	actions = create_actions();
-	append_action_output(actions, in_port, 0);
-
-	buff = create_flow_mod(get_transaction_id(), match, get_cookie(),
-			OFPFC_ADD, OFP_FLOW_PERMANENT, OFP_FLOW_PERMANENT,
-			0xffff, -1, OFPP_NONE, OFPFF_SEND_FLOW_REM, actions);
-	if (!(res = send_openflow_message(sw, buff)))
-		printf("Error installing back from host rule, res %d\n", res);
-
-	delete_actions(actions);
-	free_buffer(buff);
+	set_rule(sw, dst_mac, src_mac, in_port, tcp_dst_port, tcp_src_port);
 }
 
 /* Install rule to forward flow on the next port */
@@ -191,62 +160,14 @@ static void packet_from_host(int sw, char *src_mac, char *dst_mac,
 
 
 	/* Install rule to forward flow on the next port */
-	memset(&match, 0, sizeof(struct ofp_match));
-	match.wildcards = OFPFW_ALL;
-	match.wildcards ^= OFPFW_DL_SRC;
-	memcpy(match.dl_src, src_mac, 6);
-	match.wildcards ^= OFPFW_TP_SRC;
-	match.tp_src = tcp_src_port;
-	match.wildcards ^= OFPFW_TP_DST;
-	match.tp_dst = tcp_dst_port;
-	match.wildcards ^= OFPFW_NW_PROTO;
-	match.nw_proto = IPPROTO_TCP;
-	match.wildcards ^= OFPFW_DL_TYPE;
-	match.dl_type = 0x0800;
+	set_rule(sw, src_mac, dst_mac, next_port, tcp_src_port, tcp_dst_port);
 	printf("GOTO %d, for tcp src port %d, and mac\n", next_port, tcp_src_port);
 	print_mac(src_mac);
-
-	actions = create_actions();
-	append_action_output(actions, next_port, 0);
-
-	buff = create_flow_mod(get_transaction_id(), match, get_cookie(),
-			OFPFC_ADD, OFP_FLOW_PERMANENT, OFP_FLOW_PERMANENT,
-			0xffff, -1, OFPP_NONE, OFPFF_SEND_FLOW_REM, actions);
-	if (!(res = send_openflow_message(sw, buff)))
-		printf("Error installing alternate rule, res %d\n", res);
-
-	delete_actions(actions);
-	free_buffer(buff);
-
 
 	/* Install rule to forward packets back to the host */
 	printf("Also install rule to go back to host\n");
 	memset(&match, 0, sizeof(struct ofp_match));
-	match.wildcards = OFPFW_ALL;
-	match.wildcards ^= OFPFW_DL_DST;
-	match.wildcards ^= OFPFW_DL_SRC;
-	match.wildcards ^= OFPFW_TP_SRC;
-	match.wildcards ^= OFPFW_TP_DST;
-	memcpy(match.dl_dst, src_mac, 6);
-	memcpy(match.dl_src, dst_mac, 6);
-	match.tp_src = tcp_dst_port;
-	match.tp_dst = tcp_src_port;
-	match.wildcards ^= OFPFW_NW_PROTO;
-	match.nw_proto = IPPROTO_TCP;
-	match.wildcards ^= OFPFW_DL_TYPE;
-	match.dl_type = 0x0800;
-
-	actions = create_actions();
-	append_action_output(actions, 1, 0);
-
-	buff = create_flow_mod(get_transaction_id(), match, get_cookie(),
-			OFPFC_ADD, OFP_FLOW_PERMANENT, OFP_FLOW_PERMANENT,
-			0xffff, -1, OFPP_NONE, OFPFF_SEND_FLOW_REM, actions);
-	if (!(res = send_openflow_message(sw, buff)))
-		printf("Error installing back from host rule, res %d\n", res);
-
-	delete_actions(actions);
-	free_buffer(buff);
+	set_rule(sw, dst_mac, src_mac, 1, tcp_dst_port, tcp_src_port);
 }
 
 /* Handler for packet in event */
